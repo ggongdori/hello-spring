1c4bb541062a320980e1184000d8bb695d5c8498
ggongdori <soominlee0410@gmail.com>
2022-04-07 15:06:07 +0900

    dependency injection, 직접 스프링 빈 등록 코드 추가

--- hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
+++ hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
@@ -1,4 +1,22 @@
 package hello.hellospring.controller;
 
+
+import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class MemberController {
+
+    //필드 주입, 별로임, 바꿀 수 있는 방법이 없다
+    @Autowired
+    private MemberService memberService;
+
+
+        //Dependency Injection 3가지 -> 생성자 주입
+//    @Autowired
+//    public MemberController(MemberService memberService) {
+//
+//        this.memberService = memberService;
+//    }
 }
