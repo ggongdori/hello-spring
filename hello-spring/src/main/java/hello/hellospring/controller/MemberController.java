f4886d7cff11ac30d5ee27d6eedaf40090ede703
ggongdori <soominlee0410@gmail.com>
2022-04-07 15:08:42 +0900

    Dependency Injection 필드주입, 생성자 주입, setter 주입 코드 추가

--- hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
+++ hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
@@ -12,8 +12,14 @@ public class MemberController {
     @Autowired
     private MemberService memberService;
 
+    //setter 주입, 단점: 누군가가 호출했을 때 public으로 열려있어야함
+    //바꿀 일은 없지만 어쨌든 public으로 노출이 되어야 함, 잘못 바꾸면 문제
+   @Autowired
+    public void setMemberService(MemberService memberService) {
+        this.memberService = memberService;
+    }
 
-        //Dependency Injection 3가지 -> 생성자 주입
+    //Dependency Injection 3가지 중 -> 생성자 주입
 //    @Autowired
 //    public MemberController(MemberService memberService) {
 //
